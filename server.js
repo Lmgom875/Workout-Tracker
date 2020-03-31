d7e63c687683a6fc2d14d50971a6e15a924bfc3c
lmgom875 <lmgom875@gmail.com>
2020-03-30 23:52:01 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:passw0rd@ds159926.mlab.com:59926/heroku_5gflmvpn", {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 });
